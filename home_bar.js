d4977c5219faad9babd2d30f7aff4c219e75ad4c
Alex Price <posixoddity@gmail.com>
2014-12-12 16:12:25 -0600

    Updated data visualization slide
    Added a bar graph, need to start work on the no-code slide.

--- home_bar.js
+++ home_bar.js
@@ -1,21 +1,46 @@
 var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+		var lineChartData = {
+			labels : ["January","February","March","April","May","June","July"],
+			datasets : [
+				{
+					label: "My Second dataset",
+					fillColor : "rgba(255,87,72,0.5)",
+					strokeColor : "rgba(255,87,72,1)",
+					pointColor : "rgba(255,87,72,1)",
+					pointStrokeColor : "#fff",
+					pointHighlightFill : "#fff",
+					pointHighlightStroke : "rgba(151,187,205,1)",
+					data : [4,32,28,40,89,120]
+				}
+			]
+
+		}
+
+	window.onload = function(){
+		var ctx = document.getElementById("canvas").getContext("2d");
+		window.myLine = new Chart(ctx).Line(lineChartData, {
+			responsive: true
+		});
+	}
+
+/*var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
 
 	var barChartData = {
-		labels : ["January","February","March","April","May","June","July"],
+		labels : ["$10","$50","$100"],
 		datasets : [
 			{
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,0.8)",
-				highlightFill: "rgba(220,220,220,0.75)",
+				fillColor : "rgba(220,220,220,1)",
+				strokeColor : "rgba(220,220,220,1)",
+				highlightFill: "rgba(220,220,220,1)",
 				highlightStroke: "rgba(220,220,220,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
 			},
 			{
-				fillColor : "rgba(151,187,205,0.5)",
-				strokeColor : "rgba(151,187,205,0.8)",
-				highlightFill : "rgba(151,187,205,0.75)",
-				highlightStroke : "rgba(151,187,205,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+				fillColor : "rgba(255,87,72,1)",
+				strokeColor : "rgba(255,87,72,1)",
+				highlightFill : "rgba(255,87,72,1)",
+				highlightStroke : "rgba(255,87,72,1)",
+				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
 			}
 		]
 
@@ -26,3 +51,4 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
 			responsive : true
 		});
 	}
+*/
